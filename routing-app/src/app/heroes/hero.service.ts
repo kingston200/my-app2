990448f3a0919607693596c4c17c4d5ad3a7d206
king <kingjames2009123@outlook.com>
2024-10-30 11:36:45 -0400

    eee

---
+++ routing-app/src/app/heroes/hero.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+@Injectable({
+  // declares that this service should be created
+  // by the root application injector.
+  providedIn: 'root',
+})
+export class HeroService {
+  getHeroes() {
+    return HEROES;
+  }
+}
